c53ec097f9599f892a672cfc885c57e87461a57e
marceoroq <oroquieta.m@gmail.com>
2025-08-01 15:19:34 -0300

    refactor(constants): move constants to dedicated directory and clean up unused code

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Mona_Sans } from "next/font/google";
 import "./globals.css";
 
-import { APP_DESCRIPTION, APP_NAME } from "@/lib/constants";
+import { APP_DESCRIPTION, APP_NAME } from "@/constants";
 
 const monaSans = Mona_Sans({
   variable: "--font-mona-sans",
--- lib/constants.ts
+++
@@ -1,3 +0,0 @@
-export const APP_NAME = process.env.APP_NAME || "My App";
-export const APP_DESCRIPTION =
-  process.env.APP_DESCRIPTION || "My App Description";
