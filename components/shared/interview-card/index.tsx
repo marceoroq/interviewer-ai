8b2b36bf884725c29b047891bb26200406df8d50
marceoroq <oroquieta.m@gmail.com>
2025-08-03 15:04:52 -0300

    feat(interview-card): add techstack icons with hover tooltips

--- components/shared/interview-card/index.tsx
+++ components/shared/interview-card/index.tsx
@@ -1,11 +1,14 @@
+import Link from "next/link";
+import dayjs from "dayjs";
+import Image from "next/image";
+import { CalendarDaysIcon, StarIcon } from "lucide-react";
+
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
+
+import { getDevIconUrl } from "@/lib/utils";
 import { interviewCovers } from "@/constants";
 import { Feedback, Interview } from "@/types";
-import dayjs from "dayjs";
-import { CalendarDaysIcon, StarIcon } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
 
 export const InterviewCard = ({ interview }: { interview: Interview }) => {
   const feedback = null as Feedback | null;
@@ -41,20 +44,20 @@ export const InterviewCard = ({ interview }: { interview: Interview }) => {
       </CardContent>
       <CardFooter className="flex justify-between">
         <div className="flex">
-          <Image
-            className="bg-[#1A1C2A] border border-[#242633] rounded-full p-2"
-            width={40}
-            height={40}
-            src="/react.svg"
-            alt=""
-          />
-          <Image
-            className="bg-[#1A1C2A] border border-[#242633] rounded-full p-2 -ml-1"
-            width={40}
-            height={40}
-            src="/tailwind.svg"
-            alt=""
-          />
+          {interview.techstack.map((tech) => (
+            <div className="group rounded-full relative -ml-1 first:ml-0" key={tech}>
+              <Image
+                className="bg-[#1A1C2A] group border border-[#242633] aspect-square rounded-full p-2 -ml-1 first:ml-0"
+                width={40}
+                height={40}
+                src={getDevIconUrl(tech) || "/tech.svg"}
+                alt={tech}
+              />
+              <span className="absolute bottom-full mb-1 hidden group-hover:flex px-2 py-1 text-xs text-white bg-gray-700 rounded-md shadow-md">
+                {tech}
+              </span>
+            </div>
+          ))}
         </div>
         <Button asChild>
           <Link
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+import { techMappings } from "@/constants";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+const devIconBaseUrl =
+  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/{icon}/{icon}-original.svg";
+
+export function getDevIconUrl(icon: string) {
+  const normalizedTechName = techMappings[icon.toLowerCase()];
+  if (normalizedTechName) return devIconBaseUrl.replaceAll("{icon}", normalizedTechName);
+  return null;
 }
