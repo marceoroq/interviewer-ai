057f57ad041eb98be9b0bee8a0bea8295a782ec9
marceoroq <oroquieta.m@gmail.com>
2025-08-03 13:06:33 -0300

    feat(home): add banner component with call to action

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,10 @@
+import { Banner } from "@/components/shared/home/banner";
+
 export default function HomePage() {
-  return <div>Home page</div>;
+  return (
+    <section className="flex max-w-7xl mx-auto w-full px-8 xl:px-4 py-4 flex-col h-screen">
+      <Banner />
+      Home page
+    </section>
+  );
 }
---
+++ components/shared/home/banner/index.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export const Banner = () => {
+  return (
+    <div className="flex flex-col md:flex-row gap-6 bg-gradient-to-b from-[#171532] to-[#08090D] rounded-3xl px-16 py-6 items-center justify-between max-sm:px-4">
+      <div className="flex flex-col gap-6 lg:max-w-lg">
+        <h2 className="text-3xl font-semibold">
+          Get Interview-Ready with AI-Powered Practice & Feedback
+        </h2>
+        <p className="text-lg text-accent-foreground">
+          Practice on real interview questions and get feedback from AI
+        </p>
+        <Button className="w-full md:w-fit px-8">Start an Interview</Button>
+      </div>
+      <Image
+        src="/robot.png"
+        alt="robot image"
+        width={442}
+        height={290}
+        className="max-sm:hidden w-56 lg:w-96"
+      />
+    </div>
+  );
+};
