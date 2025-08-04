9294b8fdcf5083b0f7b1e96cd4b5ce52165f0dc2
marceoroq <oroquieta.m@gmail.com>
2025-08-04 15:13:52 -0300

    feat(firebase): add firebase integration with admin and client SDKs

---
+++ lib/firebase/admin.ts
@@ -0,0 +1,26 @@
+import { ServiceAccount } from "firebase-admin";
+import { cert, getApps, initializeApp } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
+
+const serviceAccount: ServiceAccount = {
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY,
+};
+
+export function getFirebaseAdmin() {
+  const apps = getApps();
+  if (!apps.length) {
+    initializeApp({
+      credential: cert(serviceAccount),
+    });
+  }
+
+  return {
+    auth: getAuth(),
+    db: getFirestore(),
+  };
+}
+
+export const { auth, db } = getFirebaseAdmin();
