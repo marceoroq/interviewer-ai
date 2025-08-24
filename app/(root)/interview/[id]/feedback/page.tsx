3bb45536c809928999e2d7cad71dd2bf49a0e960
marceoroq <oroquieta.m@gmail.com>
2025-08-24 19:00:49 -0300

    feat(interview-feedback): improve feedback page layout and add interview role

--- app/(root)/interview/[id]/feedback/page.tsx
+++ app/(root)/interview/[id]/feedback/page.tsx
@@ -2,13 +2,18 @@ import { InterviewService } from "@/services/interview.service";
 
 export default async function FeedbackDetailsPage({ params }: { params: Promise<{ id: string }> }) {
   const id = (await params).id;
+
   const feedback = await InterviewService.getFeedback(id);
   if (!feedback) {
     return <div>Feedback not found</div>;
   }
+
+  const interview = await InterviewService.getInterviewById(id)!;
+
   return (
-    <div>
+    <section className="section max-w-3xl text-center">
+      <h1 className="text-5xl font-bold mb-4">Feedback on the Interview — {interview?.role}</h1>
       <pre>{JSON.stringify(feedback, null, 2)}</pre>
-    </div>
+    </section>
   );
 }
