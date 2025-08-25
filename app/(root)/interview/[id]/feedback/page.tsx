f9f9d28c30816c7b54bc72f9c168fdc5fd015f8f
marceoroq <oroquieta.m@gmail.com>
2025-08-25 11:37:59 -0300

    feat(feedback): improve feedback page layout and add separator component

--- app/(root)/interview/[id]/feedback/page.tsx
+++ app/(root)/interview/[id]/feedback/page.tsx
@@ -1,4 +1,7 @@
+import { Separator } from "@/components/ui/separator";
 import { InterviewService } from "@/services/interview.service";
+import dayjs from "dayjs";
+import { CalendarDaysIcon, StarIcon } from "lucide-react";
 
 export default async function FeedbackDetailsPage({ params }: { params: Promise<{ id: string }> }) {
   const id = (await params).id;
@@ -11,9 +14,75 @@ export default async function FeedbackDetailsPage({ params }: { params: Promise<
   const interview = await InterviewService.getInterviewById(id)!;
 
   return (
-    <section className="section max-w-3xl text-center">
-      <h1 className="text-5xl font-bold mb-4">Feedback on the Interview — {interview?.role}</h1>
-      <pre>{JSON.stringify(feedback, null, 2)}</pre>
+    <section className="section h-fit max-w-4xl pb-20">
+      <h1 className="text-5xl font-bold mb-4 text-center">
+        Feedback on the Interview — {interview?.role}
+      </h1>
+
+      <div className="flex justify-center gap-6">
+        <div className="flex text-lg gap-2 items-center text-accent-foreground">
+          <StarIcon className="w-5 h-5" />
+          <p>
+            Overall Impressiones:{" "}
+            <span className="font-bold">{feedback?.totalScore || "--- "}</span>/100
+          </p>
+        </div>
+        <div className="flex gap-2 items-center text-accent-foreground">
+          <CalendarDaysIcon className="w-5 h-5" />
+          {dayjs(interview?.createdAt).format("MMM D, YYYY - h:mm A")}
+        </div>
+      </div>
+
+      <Separator />
+
+      <div className="px-16">
+        <p className="mb-4 leading-6.5">{feedback?.finalAssessment}</p>
+
+        <div className="flex flex-col">
+          <h2 className="text-3xl font-bold mb-4">Breakdown of Evaluation:</h2>
+          <ol className="list-decimal ml-8">
+            {feedback.categoryScores.map((category) => (
+              <li key={category.name} className="text-lg font-bold">
+                <h3 className="mb-2 text-orange-200">
+                  {category.name} ({category.score}/100)
+                </h3>
+                <p className="text-base leading-7 font-normal mb-6">{category.comment}</p>
+              </li>
+            ))}
+          </ol>
+        </div>
+
+        {feedback.strengths.length > 0 && (
+          <div className="mb-8">
+            <h3 className="text-2xl font-bold mb-4">Strenghts:</h3>
+            <ul className="ml-8">
+              {feedback.strengths.map((strength) => (
+                <li className="list-disc mb-2" key={strength}>
+                  {strength}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+
+        {feedback.areasForImprovement.length > 0 && (
+          <div className="mb-8">
+            <h3 className="text-2xl font-bold mb-4">Areas for Improvement:</h3>
+            <ul className="ml-8">
+              {feedback.areasForImprovement.map((strength) => (
+                <li className="list-disc mb-2" key={strength}>
+                  {strength}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+
+        <p className="text-3xl font-bold">
+          Final Veredict:{" "}
+          <span className="text-red-400 bg-slate-700 py-1 px-4 rounded-full">Not Recommended</span>
+        </p>
+      </div>
     </section>
   );
 }
