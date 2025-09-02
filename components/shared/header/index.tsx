7d8bd49113a7e98c7e68a681ebbdca7b95207dad
marceoroq <oroquieta.m@gmail.com>
2025-09-02 14:20:40 -0300

    refactor(header): move logo link styling to parent element

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -14,15 +14,14 @@ export const Header = async () => {
   return (
     <header className="flex items-center">
       <div className="flex w-full justify-between max-w-7xl mx-auto py-4 px-8 xl:px-4">
-        <Link href="/">
-          <div className="flex items-center gap-3">
-            <Image src="/logo.svg" width={38} height={32} alt="app logo" />
-            <h2 className="font-medium text-xl">{APP_NAME}</h2>
-          </div>
+        <Link href="/" className="flex items-center gap-3">
+          <Image src="/logo.svg" width={38} height={32} alt="app logo" />
+          <h2 className="font-medium text-xl">{APP_NAME}</h2>
         </Link>
 
         <div className="flex gap-4 items-center">
           <SignoutButton />
+
           <Avatar className="size-12">
             <AvatarImage src={user!.avatar!} />
             <AvatarFallback className="text-2xl">
